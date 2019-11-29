5a3f342e6f97ec59351987ed79f24d34c6dbb774
Natalie Santana <44982923+N8tlee@users.noreply.github.com>
2019-11-28 21:36:34 -0500

    flavors selection

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,23 @@
-console.log("American Pie");
+/*
+const msg = document.createElement("h2");
+msg.textContent = "Hello";
+document.querySelector("body").appendChild(msg);
+*/
+
+const flavors = ["vanilla", "chocolate", "cream cheese", "lemon", "cookies & cream", "marshmallow vanilla"];
+
+const flavorsElm = document.createElement("select");
+
+let option = document.createElement("option");
+option.textContent = "select";
+flavorsElm.appendChild(option);
+
+for (let i = 0; i < flavors.length; i++) {
+let option = document.createElement("option");
+option.textContent = flavors[i];
+flavorsElm.appendChild(option);
+} 
+
+console.log(flavorsElm);
+
+document.querySelector("body").appendChild(flavorsElm);
