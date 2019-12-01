d49c0172db07334715f35e30f4c80583c3990e50
Natalie Santana <44982923+N8tlee@users.noreply.github.com>
2019-12-01 08:10:09 -0500

    radio add and broken select for sponge

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,7 @@ msg.textContent = "Hello";
 document.querySelector("body").appendChild(msg);
 */
 
+
 const flavors = ["vanilla", "chocolate", "cream cheese", "lemon", "cookies & cream", "marshmallow vanilla"];
 
 const flavorsElm = document.createElement("select");
@@ -21,3 +22,20 @@ flavorsElm.appendChild(option);
 console.log(flavorsElm);
 
 document.querySelector("body").appendChild(flavorsElm);
+
+const spongeCake = ["vanilla", "chocolate", "red velvet", "strawberry"];
+
+const spongeCakeElm = document.createElement("select"); 
+
+let sponge = document.createElement("option");
+sponge.textContent = "select";
+spongeCakeElm.appendChild(sponge);
+
+for (let i = 0; i < spongeCake.length; i++) {
+    let sponge = document.createElement("option");
+    sponge.textContent = spongeCake[i]; spongeCakeElm.appendChild(sponge);
+}
+
+console.log(spongeCake);
+
+document.querySelector("body").appendChild(spongeCakeElm);
